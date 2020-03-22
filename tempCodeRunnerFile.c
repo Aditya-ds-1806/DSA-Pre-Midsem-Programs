79253d089ec44855768453091b1f4faa70985611
Aditya D.S <adithya18062000@gmail.com>
2020-03-23 02:57:43 +0530

    removed tempCoderunner

--- tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-void createCounters(struct Counter **);
-// void initializeCounters(struct Counter **, int, int);
-// void generateWaitlist(int);
-// void serveCustomers(struct Counter **, int);
-// void getNewCustomers(struct Counter **, int);
-// void printCounterStatus(struct Counter **);
-// void printWaitList();
